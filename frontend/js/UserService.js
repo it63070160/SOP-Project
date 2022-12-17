20f51ca4154059bea775b76196d69c39662d8cb4
it63070105 <111845618+it63070105@users.noreply.github.com>
2022-12-18 03:08:13 +0700

    Add edit and Delete

--- frontend/js/UserService.js
+++ frontend/js/UserService.js
@@ -1,8 +1,14 @@
-const USER_API_BASE_URL = "http://localhost:8082/user-service/users"
+const USER_API_BASE_URL = "http://localhost:8082/user-service/users/"
 
 function getUsers(){
     return axios.get(USER_API_BASE_URL);
 }
 function createUser(userInfo){
     return axios.post(USER_API_BASE_URL, userInfo);
+}
+function editUser(userInfo){
+    return axios.put(USER_API_BASE_URL, userInfo);
+}
+function deleteUser(userId){
+    return axios.delete(USER_API_BASE_URL, { data: userId });
 }
