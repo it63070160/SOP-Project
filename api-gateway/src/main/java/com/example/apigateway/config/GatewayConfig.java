bbe80192d6c90da4bbfe53844034e7fb6fcfb133
it63070105 <111845618+it63070105@users.noreply.github.com>
2022-12-22 01:11:15 +0700

    Add files via upload

---
+++ api-gateway/src/main/java/com/example/apigateway/config/GatewayConfig.java
@@ -0,0 +1,30 @@
+package com.example.apigateway.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+
+    @Autowired
+    AuthenticationFilter filter;
+
+    @Bean
+    public RouteLocator routes(RouteLocatorBuilder builder){
+        return builder.routes()
+                .route("user-service", r -> r.path("/user/**")
+                        .filters(f -> f.filter(filter))
+                        .uri("lb://user-service"))
+
+                .route("auth-service", r -> r.path("/auth/**")
+                        .filters(f -> f.filter(filter))
+                        .uri("lb://auth-service"))
+
+                .route("user-service", r -> r.path("/users/**")
+                        .uri("lb://user-service"))
+                .build();
+    }
+}
