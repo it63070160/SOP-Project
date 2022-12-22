c7889defb10d4547090414a4be22b361a56e2f95
it63070160 <71114745+it63070160@users.noreply.github.com>
2022-12-22 22:15:48 +0700

    Add files via upload

--- orderService/src/main/java/com/example/orderservice/query/rest/OrderQueryController.java
+++ orderService/src/main/java/com/example/orderservice/query/rest/OrderQueryController.java
@@ -1,15 +1,13 @@
 package com.example.orderservice.query.rest;
 
+import com.example.orderservice.query.FindOrderByBuyerQuery;
 import com.example.orderservice.query.FindOrdersQuery;
 import org.axonframework.messaging.responsetypes.ResponseTypes;
 import org.axonframework.queryhandling.QueryGateway;
 import org.hibernate.Hibernate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,5 +25,14 @@ public class OrderQueryController {
                 .query(findOrdersQuery, ResponseTypes.multipleInstancesOf(OrderRestModel.class)).join();
 //        Hibernate.initialize(orders);
         return orders;
+    }
+
+    @RequestMapping(value = "/orderbybuyer", method = RequestMethod.GET)
+    public List<OrderRestModel> userGetOrders(@RequestParam("id") String id){
+        FindOrderByBuyerQuery findOrderByBuyerQuery = FindOrderByBuyerQuery.builder()
+                .id(id)
+                .build();
+        return queryGateway.query(findOrderByBuyerQuery, ResponseTypes.multipleInstancesOf(OrderRestModel.class)).join();
+
     }
 }
