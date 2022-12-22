f0a5e34d8212819fb6d0f4eda2b114e2b779069f
it63070160 <71114745+it63070160@users.noreply.github.com>
2022-12-22 22:18:20 +0700

    Add files via upload

--- bookService/src/main/java/com/example/bookservice/query/rest/BookQueryController.java
+++ bookService/src/main/java/com/example/bookservice/query/rest/BookQueryController.java
@@ -7,6 +7,7 @@ import org.axonframework.queryhandling.QueryGateway;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -24,9 +25,23 @@ public class BookQueryController {
         return books;
     }
 
+    @RequestMapping(value = "/{userId}")
+    public List<BookRestModel> getUserBooks(@PathVariable String userId){
+        FindBooksQuery findBooksQuery = new FindBooksQuery();
+        List<BookRestModel> books = queryGateway
+                .query(findBooksQuery, ResponseTypes.multipleInstancesOf(BookRestModel.class)).join();
+        List<BookRestModel> userBooks = new ArrayList<>();
+        userBooks.clear();
+        for(BookRestModel book : books){
+            if(book.getOwnerId().equals(userId)){
+                userBooks.add(book);
+            }
+        }
+        return userBooks;
+    }
+
     @RequestMapping(value = "/bookbybookid", method = RequestMethod.GET)
-    public BookRestModel getBookById(@RequestParam("id") String id){
-        System.out.println(id);
+    public BookRestModel getBookById(@RequestParam("id") String id) {
         FindBookByIdQuery findBookByIdQuery = FindBookByIdQuery.builder().id(id).build();
         return queryGateway.query(findBookByIdQuery, ResponseTypes.instanceOf(BookRestModel.class)).join();
     }
